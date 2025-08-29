21af64684c5d6852e782c499cb39ac15b4c417df
Vinicius Santos <109561753+ViniSantosC@users.noreply.github.com>
2025-08-29 17:18:48 -0300

    Add files via upload

---
+++ tech investiment-vinicius/tech investiment-vinicius/java.js
@@ -0,0 +1,12 @@
+function redirenciona(){
+    window.location.href="https://github.com/CarlosEdu0808"
+}
+function redirenciona1(){
+    window.location.href="https://github.com/RyanM28S"
+}
+function redirenciona2(){
+    window.location.href="https://github.com/ViniSantosC"
+}
+function redirenciona3(){
+    window.location.href="https://github.com/GuilhermeBuenoDA"
+}
