47bb27aa0f4e85d474c107d1b07ebd4b5d46d69a
Ryan Matos da Silva <ryan.28.ms@gmail.com>
2025-08-30 17:35:35 -0300

    noooooooooooosa

--- script.js
+++ script.js
@@ -93,5 +93,19 @@ function mostrarInformações() {
 function Voltar() {
     localStorage.removeItem("senhaUsuario");
     localStorage.removeItem("senhaUsuario1");
-    window.location.href = "tela.html";
+    window.location.href = "CadastroData.html";
+}
+
+
+function redirenciona(){
+    window.location.href="https://github.com/CarlosEdu0808"
+}
+function redirenciona1(){
+    window.location.href="https://github.com/RyanM28S"
+}
+function redirenciona2(){
+    window.location.href="https://github.com/ViniSantosC"
+}
+function redirenciona3(){
+    window.location.href="https://github.com/GuilhermeBuenoDA"
 }
